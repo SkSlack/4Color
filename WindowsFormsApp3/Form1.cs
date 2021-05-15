3f70c2fa6a3a8c5673adcf32788e1d0c498da04a
sluckrunner <sluckrunner@gmail.com>
2021-05-16 01:07:32 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Former : Form
+    {
+        public Former()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Former.ActiveForm.BackColor = Color.Green;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Former.ActiveForm.BackColor = Color.Blue;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Former.ActiveForm.BackColor = Color.Red;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Former.ActiveForm.BackColor = Color.Yellow;
+        }
+    }
+}
